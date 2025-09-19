9c654337bd65f539df32bfae342523bd73e5bd52
yahir <s22120159@alumnos.itsur.edu.mx>
2025-09-19 09:33:24 -0600

    Se agrego el boton de limpiar el contador y mostrar una tarea

--- TI2A-PROGR.MOVIL-I-AD25--main/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ TI2A-PROGR.MOVIL-I-AD25--main/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,4 +1,5 @@
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -16,11 +17,25 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
         if (count > 0) {
-
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { showTask = false },
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
             Text("You've had $count glasses.")
         }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp),enabled = count < 10) {
-            Text("Add one")
+
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text("Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 8.dp)) {
+                Text("Clear water count")
+            }
         }
     }
 }
