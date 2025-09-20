37b87c65e25626624cf235619b052b2a9b46991c
yahir <s22120159@alumnos.itsur.edu.mx>
2025-09-19 22:14:19 -0600

    cambios en el archivo de la pantalla

--- TI2A-PROGR.MOVIL-I-AD25--main/app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ TI2A-PROGR.MOVIL-I-AD25--main/app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -3,6 +3,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+
 @Composable
 fun WellnessScreen(
     modifier: Modifier = Modifier,
